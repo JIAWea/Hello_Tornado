e373fcfe0242947c76f83369bda13d09098be8eb
Ray Wong <ray@126.com>
2019-03-22 13:12:45 +0800

    First step

---
+++ FirstAttempt/controllers/account.py
@@ -0,0 +1,19 @@
+import tornado.ioloop
+import tornado.web
+
+class LoginHandler(tornado.web.RequestHandler):
+    """
+    账户相关信息
+    """
+    def get(self, *args, **kwargs):
+        self.render('login.html',msg="")
+
+    def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        if username == "root" and password == '123':
+            self.set_cookie('xxxxxx','oooooo')
+            # self.set_secure_cookie('xxxxxx','oooooo')
+            self.redirect('/home')
+        else:
+            self.render('login.html',msg="用户名或密码错误")
